81337b12859985c89c0ed068bc3adf63bf61189d
Kinkando <TY_Sage@hotmail.com>
2023-04-02 10:56:30 +0700

    chore: initialize socket notification

--- handler/socket/notification_socket.ts
+++ handler/socket/notification_socket.ts
@@ -1,16 +1,20 @@
-import { Server, Socket } from 'socket.io';
+import logger from '@util/logger';
+import { Namespace, Server } from 'socket.io';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
 export function newNotificationSocket(io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) {
     const notificationNamespace = io.of('/notification')
     notificationNamespace.on('connection', socket => {
         socket.on('ping', () => socket.emit('pong', { message: 'pong' }))
-        // notificationSocketClients.push()
+        socket.on('join', (data: {room: string}) => {
+            socket.join(data.room)
+            logger.debug(`Client is connected to socket with id: ${socket.id}, room: ${data.room} (same with userUUID)`)
+        })
     })
 
-    return new NotificationSocket(io)
+    return new NotificationSocket(notificationNamespace)
 }
 
 export class NotificationSocket {
-    constructor( private io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any> ) {}
+    constructor( private sockets: Namespace ) {}
 }
